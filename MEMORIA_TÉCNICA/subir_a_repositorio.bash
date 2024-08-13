6ce2834caaf7cad0b05cc5bdc05612710c56ea86
jokasta57 <gpr1702@gmail.com>
2024-08-13 12:36:42 -0600

    lista de comandos y actualización de mi memoria técnica

---
+++ MEMORIA_TÉCNICA/subir_a_repositorio.bash
@@ -0,0 +1,17 @@
+git remote update origin https://github.com/jokasta57/CursoLaravel.git
+
+git remote -v
+
+git config --global user.email "gpr1702@gmail.com"
+git config --global user.name "jokasta57"
+
+token: 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
+
+
+git remote set-url origin https://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@github.com/jokasta57/Veterinaria.git #old
+
+git remote set-url origin https://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@github.com/jokasta57/CursoLaravel.git
+
+git remote set-url origin https://github.com/jokasta57/CursoLaravel.git
+
+pass: 1702gustavo
