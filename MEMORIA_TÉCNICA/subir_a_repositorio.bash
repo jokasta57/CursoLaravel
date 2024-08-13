4e4a55e75e3f2f59f67ff7548a957215699dc358
jokasta57 <gpr1702@gmail.com>
2024-08-13 12:49:55 -0600

    lista de comandos actualizada v2

--- MEMORIA_TÉCNICA/subir_a_repositorio.bash
+++ MEMORIA_TÉCNICA/subir_a_repositorio.bash
@@ -5,12 +5,12 @@ git remote -v
 git config --global user.email "gpr1702@gmail.com"
 git config --global user.name "jokasta57"
 
-token: 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
+token: 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
 
 
-git remote set-url origin https://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@github.com/jokasta57/Veterinaria.git #old
+git remote set-url origin https://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@github.com/jokasta57/Veterinaria.git 
 
-git remote set-url origin https://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@github.com/jokasta57/CursoLaravel.git
+git remote set-url origin https://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@github.com/jokasta57/CursoLaravel.git
 
 pass: 1702gustavo
 
